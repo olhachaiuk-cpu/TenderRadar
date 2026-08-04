e7b19c704cc0ad261d7defa566e0fd18d5eb916f
Olha Chaiuk <olha.chaiuk@ithoot.com>
2026-08-04 12:56:14 +0300

    feat: [#3125] added migrations

---
+++ TenderRadar.Infrastructure/Migrations/20260804090529_AddTenderSummary.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TenderRadar.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTenderSummary : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Summary",
+                table: "tenders",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Summary",
+                table: "tenders");
+        }
+    }
+}
